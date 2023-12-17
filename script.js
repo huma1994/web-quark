9ca7337a2d4fa5c54ba758321246f0c991f00d71
huma1994 <112937165+huma1994@users.noreply.github.com>
2023-12-17 19:34:57 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-function addTask() {
-  var taskInput = document.getElementById('taskInput');
-  var taskList = document.getElementById('taskList');
+document.addEventListener('DOMContentLoaded', function() {
+  // Generate the Ludo board dynamically
+  generateBoard();
+});
 
-  if (taskInput.value.trim() === '') {
-    alert('Please enter a task.');
-    return;
+function generateBoard() {
+  const board = document.getElementById('board');
+
+  for (let row = 1; row <= 11; row++) {
+    for (let col = 1; col <= 11; col++) {
+      const cell = document.createElement('div');
+      cell.className = 'cell';
+      cell.dataset.row = row;
+      cell.dataset.col = col;
+
+      // Add classes for colored cells
+      if (row % 2 === 0 && col % 2 === 1) {
+        cell.classList.add('red');
+      } else if (row % 2 === 1 && col % 2 === 0) {
+        cell.classList.add('yellow');
+      }
+
+      // Add classes for player cells
+      if ((row === 1 || row === 11) && col > 1 && col < 11) {
+        cell.classList.add('green');
+      } else if ((col === 1 || col === 11) && row > 1 && row < 11) {
+        cell.classList.add('blue');
+      }
+
+      board.appendChild(cell);
+    }
   }
-
-  var newTask = document.createElement('li');
-  newTask.textContent = taskInput.value;
-  newTask.onclick = function() {
-    this.parentNode.removeChild(this);
-  };
-
-  taskList.appendChild(newTask);
-  taskInput.value = '';
 }
